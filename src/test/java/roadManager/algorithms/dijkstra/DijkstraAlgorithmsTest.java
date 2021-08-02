9b51fe4945d8390ed84bd74941e3845ba30feeff
nuriakhmetov_am <almaz.zalj@mail.ru>
2021-08-02 15:49:28 +0300

    В тесте на работоспособность алгоритма поиска маршрута по минимальному расстоянию не было проверки на результат. Проверка на результат дописана.

--- src/test/java/roadManager/algorithms/dijkstra/DijkstraAlgorithmsTest.java
+++ src/test/java/roadManager/algorithms/dijkstra/DijkstraAlgorithmsTest.java
@@ -14,7 +14,10 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import static org.junit.Assert.*;
 
@@ -32,12 +35,47 @@ public class DijkstraAlgorithmsTest {
         Node start = new Node(523086937L, 55.7311784, 52.3950185);
         Node finish = new Node(835753789L, 55.7338274, 52.3938429);
 
+        List<Long> expected = new ArrayList<>();
+        expected.add(7685098474L);
+        expected.add(523634933L);
+        expected.add(523086968L);
+        expected.add(531867915L);
+        expected.add(904254780L);
+        expected.add(904254145L);
+        expected.add(523634951L);
+        expected.add(523634957L);
+        expected.add(7685119796L);
+        expected.add(7685119784L);
+        expected.add(523086870L);
+        expected.add(523086880L);
+        expected.add(904255068L);
+        expected.add(523086937L);
+        expected.add(534108442L);
+        expected.add(523086878L);
+        expected.add(7685119786L);
+        expected.add(904254569L);
+        expected.add(523086882L);
+        expected.add(523086907L);
+        expected.add(904254977L);
+        expected.add(904254560L);
+        expected.add(523086876L);
+        expected.add(523086908L);
+        expected.add(835753789L);
+        expected.add(7685119703L);
+        expected.add(534108441L);
+
         Route route = algorithms.calculatePath(start, finish);
 
         System.out.println("Расстояние: " + route.getLength());
         System.out.println("Время на преодоление: " + route.getTime());
         System.out.println("Двигайтесь по точкам: ");
-        route.getNodes().stream()
+        Set<Node> nodes = route.getNodes();
+        nodes.stream()
                 .forEach(node -> System.out.println("\tid: " + node.getId()));
+
+        for (Node node : nodes) {
+            if (!expected.contains(node.getId()))
+                fail();
+        }
     }
 }
