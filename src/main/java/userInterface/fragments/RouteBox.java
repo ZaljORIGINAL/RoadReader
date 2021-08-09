73431a2e8a58ecef862e391ea25cec40eabdbd85
nuriakhmetov_am <almaz.zalj@mail.ru>
2021-08-09 11:02:29 +0300

    Графический интерфейс. Сконструирована часть ответа сконструированного маршрута.

---
+++ src/main/java/userInterface/fragments/RouteBox.java
@@ -0,0 +1,14 @@
+package userInterface.fragments;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RouteBox implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
