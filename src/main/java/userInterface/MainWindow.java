2d50d37fde757fb3543ad28adc8cf2a8691b8b79
nuriakhmetov_am <almaz.zalj@mail.ru>
2021-08-09 10:00:09 +0300

    Добавлена поддержка JavaFX.

---
+++ src/main/java/userInterface/MainWindow.java
@@ -0,0 +1,14 @@
+package userInterface;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainWindow implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
