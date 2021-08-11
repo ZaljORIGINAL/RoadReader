60878fb3a78eb1321ac500f5b5b7b6be7ae84e33
nuriakhmetov_am <almaz.zalj@mail.ru>
2021-08-11 08:09:08 +0300

    Было произведено слияние веток.
    Класс MainWindow.java:
    * Стерты упоминания и обращения к удаленному классу MapData.java.

--- src/main/java/userInterface/windows/MainWindow.java
+++ src/main/java/userInterface/windows/MainWindow.java
@@ -2,13 +2,10 @@ package userInterface.windows;
 
 import algorithms.Graph;
 import algorithms.dijkstra.DijkstraAlgorithms;
-import algorithms.dijkstra.WeightTypes.TimeWeightCalculator;
-import algorithms.pointsFinders.ClosestPointFinderTree;
 import algorithms.pointsFinders.QuadTree;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import mapObjects.*;
@@ -18,7 +15,6 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ResourceBundle;
 
@@ -35,7 +31,6 @@ public class MainWindow implements Initializable {
     public Button blockedPointFileConfigSelect;
 
     private OsmParser parser;
-    private MapData mapData;
     private Graph graph;
     private QuadTree quadTree;
     private DijkstraAlgorithms algorithm;
@@ -45,14 +40,6 @@ public class MainWindow implements Initializable {
             //Фаил карты
             URL urlToMap = MainWindow.class.getResource("/toParse.osm");
             parser = new OsmParser(Paths.get(urlToMap.toURI()));
-            mapData = parser.extract();
-            quadTree = new ClosestPointFinderTree(
-                    55.63213647883612, 52.201261423294824,
-                    55.81003367465946, 52.605008981888574,
-                    6);
-            mapData.getTowerNodes()
-                    .forEach(quadTree::add);
-            graph = new Graph(mapData.getNodes(), mapData.getEdges());
             algorithm = new DijkstraAlgorithms(graph);
             } catch (ParserConfigurationException e) {
                 e.printStackTrace();
@@ -79,10 +66,5 @@ public class MainWindow implements Initializable {
         GeographicPoint finish = new GeographicPoint(
                 Double.parseDouble(finishLat.getText()),
                 Double.parseDouble(finishLon.getText()));
-
-        Route route = algorithm.calculatePath(
-                new TimeWeightCalculator(),
-                (Node) quadTree.find(start),
-                (Node) quadTree.find(finish));
     }
 }
